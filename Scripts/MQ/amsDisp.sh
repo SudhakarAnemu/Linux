2d91ff6b2ddc11388754abcc3bbe6208884dd44a
SudhakarAnemu <sudhakar.anemu@gmail.com>
2025-02-03 19:03:50 -0500

    Update amsDisp.sh

--- Scripts/MQ/amsDisp.sh
+++ Scripts/MQ/amsDisp.sh
@@ -13,3 +13,25 @@ do
     done; 
 done
 echo "-----------  Completed -----------"
+echo "----- All Policies vs QMGR"
+cat $LOG
+
+
+
+dspmqspl -m QMGR -export
+
+--------> All qmgrs : 
+
+dspmqsplAllQmgrs.sh
+
+#!/bin/bash
+LOG=/tmp/dspmqspl.log
+>$LOG
+    dspmq|awk -F"[(*)]" '{print  $2}'|while read qmgr; 
+    do    
+        echo "--- QMGR : $qmgr" >> $LOG
+        dspmqspl -m $qmgr -export >> $LOG
+    done; 
+echo "-----------  Completed -----------"
+echo -e "----- All Policies vs QMGR"
+cat $LOG

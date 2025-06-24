7c98ad14a4c208ce2b60538ef4448fea0fb2bb03
SudhakarAnemu <sudhakar.anemu@gmail.com>
2025-06-24 12:52:46 -0400

    Update MQTTUpV12Brks.sh

--- Scripts/Ace/MQTTUpV12Brks.sh
+++ Scripts/Ace/MQTTUpV12Brks.sh
@@ -23,4 +23,15 @@ done
 #mqsichangeproperties IIBDVBA34 -b Events -o AdminEvents/MQTT -n enabled -v false
 #mqsichangeproperties IIBDVBA34 -b Events -o OperationalEvents/MQ -n enabled -v false
 	
-	
+List out Properties : 
+
+for brk in `mqsilist|grep running|awk -F"'" '{print $2}'`; do echo "-----Broker : $brk"; mqsireportproperties $brk -b Events -r | grep -i MQTT -A 1 ; done
+
+Update "false" for the list of Brokers : 
+
+while IFS= read -r brk; do echo "Processing: ---$brk---"; mqsichangeproperties $brk -b Events -o OperationalEvents/MQTT -n enabled -v false;mqsichangeproperties $brk -b Events -o BusinessEvents/MQTT -n enabled -v false;mqsichangeproperties $brk -b Events -o AdminEvents/MQTT -n enabled -v false;mqsichangeproperties $brk -b Events -o OperationalEvents/MQ -n enabled -v false; done < brks.txt
+
+
+Update "false" for all Brokers : 
+
+for brk in `mqsilist|grep running|awk -F"'" '{print $2}'`; do echo "-----Broker : $brk";  mqsichangeproperties $brk -b Events -o OperationalEvents/MQTT -n enabled -v false;mqsichangeproperties $brk -b Events -o BusinessEvents/MQTT -n enabled -v false;mqsichangeproperties $brk -b Events -o AdminEvents/MQTT -n enabled -v false;mqsichangeproperties $brk -b Events -o OperationalEvents/MQ -n enabled -v false;  done
